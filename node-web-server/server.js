57eb1e0a82d007c181fc2dc22226a0e49fc4c3c5
Seul Zarraga Espinosa <seulzarragaespinosa@Seuls-MacBook-Pro.local>
2017-11-20 17:29:47 -0600

    advanced templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () =>{
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) =>{
+
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) =>{
-	res.send('hello world');
+	res.render('home.hbs', {
+		pageTitle: 'Home',
+		welcomeMessage: 'Welcome to Tijuana'
+	});
 });
 
 app.get('/about', (req, res) =>{
-	res.send('<h1>About this page</h1>');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
 });
 
 app.get('/bad', (req, res) =>{
@@ -17,4 +34,6 @@ app.get('/bad', (req, res) =>{
 	});
 });
 
-app.listen(3000);
+app.listen(3000, () =>{
+	console.log('server is up on port 3000!');
+});
